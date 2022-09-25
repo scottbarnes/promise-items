4de1cd2fb131b7860fc7f25d317a56d0ab14ba49
Scott Barnes <scottreidbarnes@gmail.com>
2022-09-24 20:32:48 -0700

    Just after beginning to refactor for BatchStats

--- promise/main.py
+++ promise/main.py
@@ -1,12 +1,15 @@
 import requests
-from dataclasses import dataclass
+import pickle
+import time
+from datetime import datetime
+from dataclasses import dataclass, field
 from itertools import islice
-from typing import Final, TypeVar
-from collections.abc import Iterator
+from typing import Final, Literal
+from collections.abc import Iterator, Iterable
 
 API_URL: Final = "https://archive.org/advancedsearch.php?q=collection%3Aprotodonationitems&fl%5B%5D=identifier&sort%5B%5D=addeddate+desc&sort%5B%5D=&sort%5B%5D=&rows=1&page=1&output=json"  # noqa E501
 OL_SOLR_URL: Final = "https://openlibrary.org/search.json?fields=isbn&q=isbn:(%s)"
-T = TypeVar("T")
+ADD_BOOK_BY_ISBN_URL: Final = "https://openlibrary.org/isbn/%s"
 QueryType = dict[str, int | list[str] | str]
 
 
@@ -15,36 +18,98 @@ class Batch:
     """
     Batch stores the batch information for a single batch of promise item ISBNs.
 
-    {isbns}: the set of isbns in this batch
-    {total}: the total number of isbns in the batch
-    {isbns_in_ol}: the number of ISBNs from the batch that are in Open Library
-    {isbns_not_in_ol}: the number of ISBNs from the batch that are NOT in Open Library
+    NOTE: All values are based on the last run of {self.check_if_isbns_in_openlibrary()}
+
+    {promise_item_isbns}: the set of isbns in this batch.
+    {hits}: the set of ISBNs from this batch known to be in Open Library.
+    {misses}: the set of ISBNs from this batch known NOT to be in Open Library.
+    {total}: the total number of isbns in the batch.
+    {isbns_in_ol}: the number of ISBNs from the batch that are in Open Library.
+    {isbns_not_in_ol}: the number of ISBNs from the batch that are NOT in Open Library.
     """
 
-    isbns: set[str]
+    promise_item_isbns: set[str]
+    hits: set[str] = field(default_factory=set)
+    misses: set[str] = field(default_factory=set)
     total: int = 0
-    isbns_in_ol: int = 0
-    isbns_not_in_ol: int = 0
+    in_ol_count: int = 0
+    not_in_ol_count: int = 0
 
     def __post_init__(self) -> None:
-        self.total = len(self.isbns)
+        self.total = len(self.promise_item_isbns)
 
-    def process_isbns(self) -> None:
+    def check_if_isbns_in_openlibrary(
+        # self, items: Literal["promise_items", "misses"]
+        self,
+    ) -> None:
         """
-        Process {batch} by turning its ISBNs into a Solr query, querying Solr, using
-        get_query_isbns() to get all the Solr ISBN results, and then updating
-        {batch.isbns_in_ol} and {batch.isbns_not_in_ol}.
+        Check if either {self.promise_items} or {selfmisses} are in the Open Library
+        database.
+
+        Process either {self.promise_items} or {self.misses} by turning their ISBNs
+        into a Solr query. After query Solr, use get_query_isbns() grab all the ISBNs
+        from the Solr response, and then update all the values in {batch} accordingly.
         """
-        # Create the Solr query.
-        query = " OR ".join(self.isbns)
+        # Update values based on whether we're updating {promise_items} or {misses}.
+        query = " OR ".join(self.promise_item_isbns)
         query_result = requests.get(OL_SOLR_URL % query).json()
         isbns_from_query = get_query_isbns(query_result)
+        self.hits = self.promise_item_isbns.intersection(isbns_from_query)
+        self.misses = self.promise_item_isbns - isbns_from_query
+        self.in_ol_count = len(self.hits)
+        self.not_in_ol_count = len(self.misses)
 
-        # Get the ISBNS already in Solr (i.e. the intersection of {batch.isbns} and
-        # {isbns_from_query}) to update {batch} with what's in Open Library and what's not.
-        intersection = self.isbns.intersection(isbns_from_query)
-        self.isbns_in_ol = len(intersection)
-        self.isbns_not_in_ol = self.total - self.isbns_in_ol
+        # # Update values based on whether we're updating {promise_items} or {misses}.
+        # match items:
+        #     case "promise_items":
+        #         query = " OR ".join(self.promise_item_isbns)
+        #         query_result = requests.get(OL_SOLR_URL % query).json()
+        #         isbns_from_query = get_query_isbns(query_result)
+        #         self.hits = self.promise_item_isbns.intersection(isbns_from_query)
+        #         self.misses = self.promise_item_isbns - isbns_from_query
+        #         self.in_ol_count = len(self.hits)
+        #         self.not_in_ol_count = len(self.misses)
+        #     case "misses":
+        #         query = " OR ".join(self.misses)
+        #         query_result = requests.get(OL_SOLR_URL % query).json()
+        #         isbns_from_query = get_query_isbns(query_result)
+        #         self.hits.update(self.promise_item_isbns.intersection(isbns_from_query))
+        #         self.misses = self.misses - isbns_from_query
+        #         self.in_ol_count = len(self.hits)
+        #         self.not_in_ol_count = len(self.misses)
+
+    def add_misses(self, delay_in_ms: int):
+        """
+        Attempt to add items from {self.misses} to Open Library via
+        https://openlibrary.org/isbn/{isbn}. {delay_in_ms} specifies the dalay in
+        milliseconds between add attempts.
+        """
+        sleep_time = delay_in_ms / 1000
+        for miss in self.misses:
+            r = requests.get(ADD_BOOK_BY_ISBN_URL % miss)
+            if r.status_code == 200:
+                self.misses.remove(miss)
+            time.sleep(sleep_time)
+
+
+@dataclass
+class BatchStats:
+    """Record the stats for the batch as a whole. Use the misses to inform new batches."""
+
+    promise_item_isbns: set[str] = field(default_factory=set)
+    hits: set[str] = field(default_factory=set)
+    misses: set[str] = field(default_factory=set)
+    total: int = 0
+    in_ol_count: int = 0
+    not_in_ol_count: int = 0
+    last_run: datetime = datetime.now()
+
+    def loader(self, batches: Batch):
+        """Uses the already processed {batches} to populate the values."""
+        for batch in batches:
+            self.promise_item_isbns.update(batch.promise_item_isbns)
+            self.hits.update(batch.hits)
+            self.misses.update(batch.misses)
 
 
 def get_promise_item_metadata_url(API_URL: str) -> str:
@@ -72,7 +137,7 @@ def make_batches(isbns: Iterator[str], size: int) -> Iterator[Batch]:
     contents of each batch's ISBNs for later processing.
     """
     while batch_isbns := set(islice(isbns, size)):
-        yield Batch(isbns=batch_isbns)
+        yield Batch(promise_item_isbns=batch_isbns)
 
 
 def get_query_isbns(query_result: QueryType) -> set[str]:
@@ -115,17 +180,17 @@ def main() -> None:
     isbns = get_promise_item_isbns(url)
     batches = make_batches(isbns, 100)
 
-    # Because batch.isbns holds each batch's ISBNs, this list could take a lot of 
+    # Because batch.isbns holds each batch's ISBNs, this list could take a lot of
     # memory if the promise items have a lot of ISBNs. One solution would be to
     # clear batch.isbns after batch.process_isbns(), as they're no longer needed.
     processed_batches = []
     for batch in batches:
-        batch.process_isbns()
+        batch.check_if_isbns_in_openlibrary()
         processed_batches.append(batch)
 
     total = sum(batch.total for batch in processed_batches)
-    total_in = sum(batch.isbns_in_ol for batch in processed_batches)
-    total_out = sum(batch.isbns_not_in_ol for batch in processed_batches)
+    total_in = sum(batch.in_ol_count for batch in processed_batches)
+    total_out = sum(batch.not_in_ol_count for batch in processed_batches)
     print(f"Total number: {total}")
     print(f"Total in: {total_in}")
     print(f"Total out: {total_out}")
--- tests/test_promise.py
+++ tests/test_promise.py
@@ -1,10 +1,13 @@
+from json import JSONEncoder
 from promise import __version__
 import pytest
+import requests_mock
 
 from promise.main import (
     make_batches,
     get_query_isbns,
     Batch,
+    BatchStats,
 )
 
 
@@ -23,7 +26,7 @@ isbns = [
 ]
 iterator_isbns = iter(isbns)
 
-query_result = {
+initial_query_result = {
     "numFound": 3,
     "start": 0,
     "numFoundExact": True,
@@ -46,17 +49,41 @@ query_result = {
     "offset": None,
 }
 
+result_after_importing_one_book = {
+    "numFound": 4,
+    "start": 0,
+    "numFoundExact": True,
+    "docs": [
+        {
+            "isbn": [
+                "2880460794",
+                "2880462703",
+                "9782880460792",
+                "9780823062010",
+                "9782880462703",
+                "0823062015",
+            ]
+        },
+        {"isbn": ["9781405892469", "1405892463"]},
+        {"isbn": ["9788189999520", "8189999524"]},
+        {"isbn": ["2723496112", "9782723496117"]},
+    ],
+    "num_found": 4,
+    "q": "isbn:(9788189999520 OR 9781405892469 OR 9782723496117 OR 9783522182676 OR 9782880462703)",
+    "offset": None,
+}
+
 
 @pytest.fixture
 def batch():
     test_isbns = set(isbns)  # Batch.isbns expects a set.
-    batch = Batch(isbns=test_isbns)
+    batch = Batch(promise_item_isbns=test_isbns)
     yield batch
 
 
 def test_batch(batch):
     """Ensure the Batch class behaves as expected."""
-    assert batch.isbns == set(isbns)
+    assert batch.promise_item_isbns == set(isbns)
     assert batch.total == 5
 
 
@@ -70,15 +97,15 @@ def test_make_batches():
     next(batches)
     batch = next(batches)
     assert isinstance(batch, Batch)
-    assert batch.isbns == {"9783522182676", "9782723496117"}
+    assert batch.promise_item_isbns == {"9783522182676", "9782723496117"}
     # Ensure last iteration has remaining item(s) if <= batch size.
-    assert next(batches).isbns == {"9782880462703"}
+    assert next(batches).promise_item_isbns == {"9782880462703"}
     with pytest.raises(StopIteration):
         next(batches)
 
 
 def test_get_query_isbns():
-    assert get_query_isbns(query_result) == {
+    assert get_query_isbns(initial_query_result) == {
         "0823062015",
         "1405892463",
         "2880460794",
@@ -92,7 +119,38 @@ def test_get_query_isbns():
     }
 
 
-def test_batch_process_isbns(batch: Batch):
+"""
+Import attempt. High level. Create a new method to do GETs to /isbn. It should
+keep track of any 200 responses it gets and remove them from self.misses.
+
+Then serialize the list[Batch] and write it to disk.
+Read from disk, deserialize to continue.
+
+make batch.check_if_isbns_in_openlibrary() take an argument (Literal) for isbns or misses. May
+need to rename self.isbns so it's more clear this is the list. Maybe
+self.promise_item_isbns.
+
+run check_if_isbns_in_openlibrary(misses).
+
+
+serialize/deserialize.
+
+I need to:
+    iterate through batch.misses
+    ensure there is a delay. Say, 1 second for now between requests.
+    make a GET to http://localhost:8080/isbn/{isbn}
+    check r.status_code
+        if 200, remove from self.misses
+        if 404, do nothing
+    update self.in_ol_count and self.not_in_ol_count
+
+create a serializer/deserializer to read/write to disk.
+
+command line arg to run import process.
+"""
+
+
+def test_batch_process_promise_item_isbns(batch: Batch):
     """
     Ensure batch.process() gets the right count.
     NOTE:
@@ -100,7 +158,106 @@ def test_batch_process_isbns(batch: Batch):
         with a different promise item or if the unknown ISBNs are later added. This
         should be mocked.
     """
-    batch.process_isbns()
-    assert batch.total == 5
-    assert batch.isbns_in_ol == 3
-    assert batch.isbns_not_in_ol == 2
+    with requests_mock.Mocker(json_encoder=JSONEncoder) as m:
+        m.get(requests_mock.ANY, json=initial_query_result)
+        batch.check_if_isbns_in_openlibrary()
+        assert batch.hits == {
+            "9781405892469",
+            "9782880462703",
+            "9788189999520",
+        }
+        assert batch.misses == {"9782723496117", "9783522182676"}
+        assert batch.total == 5
+        assert batch.in_ol_count == 3
+        assert batch.not_in_ol_count == 2
+
+
+def test_batch_stats(batch: Batch):
+    """Get the stats for all the batches."""
+    batches = make_batches(iter(isbns), 2)
+
+    processed_batches = []
+    for batch in batches:
+        batch.check_if_isbns_in_openlibrary()
+        processed_batches.append(batch)
+
+    batch_stats = BatchStats()
+    batch_stats.loader(batches=processed_batches)
+
+    assert batch_stats.promise_item_isbns == {
+        "9781405892469",
+        "9782880462703",
+        "9788189999520",
+        "9782723496117",
+        "9783522182676",
+    }
+    assert batch_stats.hits == {
+        "9781405892469",
+        "9782880462703",
+        "9788189999520",
+    }
+    assert batch_stats.misses == {"9782723496117", "9783522182676"}
+    assert batch_stats.total == 5
+    assert batch_stats.in_ol_count == 3
+    assert batch_stats.not_in_ol_count == 2
+
+
+# def test_batch_process_promise_item_isbns(batch: Batch):
+#     """
+#     Ensure batch.process() gets the right count.
+#     NOTE:
+#         This test is not mocked and does connect to live database and will fail with
+#         with a different promise item or if the unknown ISBNs are later added. This
+#         should be mocked.
+#     """
+#     with requests_mock.Mocker(json_encoder=JSONEncoder) as m:
+#         m.get(requests_mock.ANY, json=initial_query_result)
+#         batch.check_if_isbns_in_openlibrary("promise_items")
+#         assert batch.hits == {
+#             "9781405892469",
+#             "9782880462703",
+#             "9788189999520",
+#         }
+#         assert batch.misses == {"9782723496117", "9783522182676"}
+#         assert batch.total == 5
+#         assert batch.in_ol_count == 3
+#         assert batch.not_in_ol_count == 2
+
+
+# def test_batch_process_misses(batch: Batch):
+#     """
+#     Ensure batch.process("misses") updates the batch values to account for items added
+#     by batch.add_misses.
+
+#     NOTE: The mocked response here assumes {batch.add_misses()} has been run and was
+#     able to successfully add "9782723496117" to the Open Library database.
+#     """
+#     # Must run with "promise_items" first because there are no misses until this is run.
+#     # Start off with two misses.
+#     batch.check_if_isbns_in_openlibrary("promise_items")
+
+#     # No point in running these?
+#     # with requests_mock.Mocker(json_encoder=JSONEncoder) as m:
+#     #     m.get(requests_mock.ANY, json=initial_query_result)
+#     #     batch.check_if_isbns_in_openlibrary("promise_items")
+#     #     assert batch.hits == {
+#     #         "9781405892469",
+#     #         "9782880462703",
+#     #         "9788189999520",
+#     #     }
+#     #     assert batch.misses == {"9782723496117", "9783522182676"}
+
+#     # Pretend only one of the two misses is added by batch.add_misses()
+#     with requests_mock.Mocker(json_encoder=JSONEncoder) as m:
+#         m.get(requests_mock.ANY, json=result_after_importing_one_book)
+#         batch.check_if_isbns_in_openlibrary("misses")
+#         assert batch.hits == {
+#             "9781405892469",
+#             "9782723496117",
+#             "9782880462703",
+#             "9788189999520",
+#         }
+#         assert batch.misses == {"9783522182676"}
+#         assert batch.total == 5
+#         assert batch.in_ol_count == 4
+#         assert batch.not_in_ol_count == 1
